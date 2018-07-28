0db80ab8a2b8e9598f30fc8976bb415db64fb170
Elze Hamilton <elze.hamilton@gmail.com>
2018-07-28 11:28:48 -0500

    Word2Vec now reads actual job descriptions and creates a vocabulary

--- wv1.py
+++ wv1.py
@@ -24,7 +24,7 @@ config = configparser.RawConfigParser()
 
 importFileDirectory = 'C:\\Users\\elze\\Documents\\MyProjects\\Python\\SkillClustersProcessedJobPostings\\SCP3\\20180619\\0001_0200'
 
-def read_input():
+def read_input_old():
 
 	oneLineJobDescriptions = []
 	for jobFileName in listdir(importFileDirectory):
@@ -34,19 +34,40 @@ def read_input():
 
 		lines = jobDescription.split("\n")
 		jobDescriptionOneLine = ';'.join(lines);
+		wordsInASentence = gensim.utils.simple_preprocess(jobDescriptionOneLine, deacc=False, min_len=2, max_len=30)
+		
 		oneLineJobDescriptions.append(jobDescriptionOneLine)
 	return oneLineJobDescriptions
 
 
-sampleSentences = [['Experience', 'in', 'ASP.NET MVC', 'and', 'WebAPI'], ['Strong', 'JavaScript', 'background'], ['Proficiency', 'in', 'SQL Server'], ['Knowledge', 'working', 'in', 'AWS', 'Azure', 'or', 'other', 'cloud' 'services']]
+def read_input():
+
+	jobTokenLists = []
+	for jobFileName in listdir(importFileDirectory):
+		jobFileExtendedName = importFileDirectory + "\\" + jobFileName
+		jobFile = open(jobFileExtendedName, "r")
+		jobDescription = jobFile.read()
+
+		lines = jobDescription.split("\n")
+		jobDescriptionOneLine = ';'.join(lines);
+		wordsInASentence = gensim.utils.simple_preprocess(jobDescriptionOneLine, deacc=False, min_len=2, max_len=30)
+		jobTokenLists.append(wordsInASentence)
+		
+	return jobTokenLists
+
+
+#sampleSentences = [['Experience', 'in', 'ASP.NET MVC', 'and', 'WebAPI'], ['Strong', 'JavaScript', 'background'], ['Proficiency', 'in', 'SQL Server'], ['Knowledge', 'working', 'in', 'AWS', 'Azure', 'or', 'other', 'cloud' 'services']]
 # train word2vec on the two sentences
 #model = gensim.models.Word2Vec(sentences, min_count=1)
 
+jobPostingTokenLists = read_input()
 
-model = gensim.models.Word2Vec(sampleSentences, iter=10, min_count=1, size=300, workers=4)
+model = gensim.models.Word2Vec(jobPostingTokenLists, iter=10, min_count=1, size=300, workers=4)
 vocab_size = len(model.wv.vocab)
 print ("vocab_size = " + str(vocab_size))
 
 for i in range(vocab_size - 1):
-	print(model.wv.index2word[i])	
+	print(model.wv.index2word[i])
 
+#for jtl in jobPostingTokenLists:
+#	print(jtl)
